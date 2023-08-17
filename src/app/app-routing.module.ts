f4090f521a24865c866bea6504504644f640b865
Vinicius Rocha <rochavini@outlook.com>
2023-08-17 17:43:00 -0300

    Base routing and components creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { PeopleComponent } from './modules/people/people.component';
+import { DepartmentsComponent } from './modules/departments/departments.component';
+import { UsersComponent } from './modules/users/users.component';
+import { NotFoundComponent } from './navigation/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'departments', component: DepartmentsComponent },
+  { path: 'users', component: UsersComponent },
+
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: false })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/header/navigation.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
+import { PeopleComponent } from './modules/people/people.component';
+import { DepartmentsComponent } from './modules/departments/departments.component';
+import { UsersComponent } from './modules/users/users.component';
+import { NotFoundComponent } from './navigation/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HomeComponent,
+    PeopleComponent,
+    DepartmentsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
